a990c95fc619bd1878c14cad5bf49365134c35f8
Clint Savage <herlo1@gmail.com>
2011-08-09 11:51:26 -0600

    simple print reporting for now

--- chase/pychase.py
+++ chase/pychase.py
@@ -36,30 +36,82 @@ class PyChase(cmd.Cmd):
 
         tag = args.tag
         pkg_list = args.pkgs
-        tag_id = None
-        if tag:
-            tag_id = self.session.search(tag, 'tag', 'glob')[0]['id']
-#        print "tag: %s, tag_id: %s" % (tag, tag_id)
+
+        #set the initial values to false
+        show_passed = False
+        show_failed = False
+        show_unbuilt = False
+
+        # if nothing is set, show all results
+        if not args.passed and not args.failed and not args.unbuilt:
+            show_passed = True
+            show_failed = True
+            show_unbuilt = True
+
+        # otherwise, show only the results *if* they are set 
+        if args.passed: show_passed = True
+        if args.failed: show_failed = True
+        if args.unbuilt: show_unbuilt = True
+
+        tag_id = self.session.search(tag, 'tag', 'glob')[0]['id']
 
         pkgs = list()
         if pkg_list:
             for pkg in pkg_list:
                 pkg_id = self.session.search(pkg, 'package', 'glob')[0]['id']
-                pkgs.append(self.session.listPackages(pkgID=pkg_id, tagID=tag_id))
+                builds = self.session.listPackages(pkgID=pkg_id, tagID=tag_id)
+#                print "pkgsX: %s" % pkgs
+                for b in builds:
+#                    print 'pkgsY: %s' % b
+                    pkgs.append(b)
 
-            pkgs = pkgs[0]
+#            print "pkgs B4: %s" % pkgs
 
         else:
             pkgs = self.session.listPackages(tagID=tag_id)
 
+#            print "pkgs B4: %s" % pkgs
+
+        passed = []
+        passed_count = 0
+        failed = []
+        failed_count = 0
+        unbuilt = []
+        unbuilt_count = 0
+
         for p in pkgs:
-#            print "name: %s" % p['package_name']
-#            print "id: %d" % p['package_id']
+#            print "pkginfo: %s" % p
             p_info = self.session.listBuilds(packageID=p['package_id'])
+#            print "p_info: %s" % p_info
             for i in p_info:
                 if i['release'].endswith('gl6'):
-                    print i['nvr']
                     if i['state'] == 1:
-                        print "succeeded"
+                        passed.append(i)
+                        passed_count += 1
                     else:
-                        print "failed"
+                        failed.append(i)
+                        failed_count += 1
+                else:
+                    unbuilt.append(i)
+                    unbuilt_count += 1
+
+        # report results
+        print "=== Build Information Report ===\n"
+        # print passed packages
+        if show_passed:
+
+            for p in passed:
+                print "  %s\t\t\tPASSED" % p['nvr'] 
+        if show_failed:
+            pass
+        if show_unbuilt:
+            pass
+
+        print "Total Passed: %d" % passed_count
+        print " Total Failed: %d" % failed_count
+        print " Total Unbuilt: %d" % unbuilt_count
+
+
+
+
+
